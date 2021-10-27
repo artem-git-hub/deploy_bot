7862000e494c92df54dde6acb24761b7a921e149
Artem Arehov <pro100adresrus@gmail.com>
2021-10-27 11:38:31 +0300

    Good edits

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ..
+cd ~
 mkdir bot
-cd bot
+cd ~/bot
 echo "----------Start deploy!----------"
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
@@ -14,16 +14,14 @@ git clone https://github.com/artem-git-hub/start_files
 git clone https://github.com/artem-git-hub/telebot.postgres
 mv start_files/photo/ telebot.postgres/
 mv start_files/venv/ telebot.postgres/
-cd telebot.postgres
-source venv/bin/activate
-mv ~/deploy_bot/bot.conf /etc/supervisor/conf.d/
+source ~/telebot.postgres/venv/bin/activate
+mv -f ~/deploy_bot/bot.conf /etc/supervisor/conf.d/
 mv -f ~/deploy_bot/supervisord.conf /etc/supervisor/
 sudo -u postgres psql --command "CREATE USER shopbot WITH CREATEDB PASSWORD 'shopmebot';"
 sudo -u postgres psql --command "ALTER ROLE shopbot SUPERUSER;"
 sudo -u postgres psql --command "CREATE DATABASE shop;"
 sudo -u postgres psql -d shop < ../start_files/shop.postgres2.sql;
 sudo -u postgres psql --command "GRANT ALL PRIVILEGES ON DATABASE shop TO shopbot;"
-echo "http://37.140.198.172:9001/    -   -    управление ботом  "
 cd ..
 rm -r start_files
 cd ..
@@ -36,3 +34,8 @@ pip install psycopg2-binary
 pip install PyTelegramBotAPI
 sudo service supervisor reread
 sudo service supervisor update
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo systemctl stop supervisor
+sudo systemctl start supervisor
+cd ~
